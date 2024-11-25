a7911e7873d95806b3981ee98fe79aad2715152d
Diae Hajib <diae.hajib@dedalus.eu>
2024-11-25 13:49:28 +0100

    update

--- scripts/AutoSetupExpress.ps1
+++ scripts/AutoSetupExpress.ps1
@@ -6,3 +6,4 @@ mkdir src/services
 mkdir src/helpers
 mkdir src/types
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/hajibDiae/templates-repo/main/Express/index.ts" -OutFile "./src/index.ts"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/hajibDiae/templates-repo/main/Express/mainController.ts" -OutFile "./src/controllers/mainController.ts"
