a833a4ab60e164dcddccc1d192fb7df9abca23ea
Diae Hajib <diae.hajib@dedalus.eu>
2024-11-25 13:25:02 +0100

    update

--- scripts/AutoSetupExpress.ps1
+++ scripts/AutoSetupExpress.ps1
@@ -1,4 +1,3 @@
-npm init -y
 npm install axios express @types/express cors
 mkdir src
 mkdir src/controllers
@@ -6,3 +5,4 @@ mkdir src/middleware
 mkdir src/services
 mkdir src/helpers
 mkdir src/types
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/hajibDiae/templates-repo/main/Express/index.ts" -OutFile "./src/index.ts"
