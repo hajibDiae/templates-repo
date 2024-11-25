8974488b262e1aa6f60ed788399e2942f194fac2
Diae Hajib <diae.hajib@dedalus.eu>
2024-11-25 13:32:12 +0100

    update

--- Express/index.ts
+++ Express/index.ts
@@ -1,1 +1,45 @@
-console.log("hello world");
+import Express from "express";
+import cors from "cors";
+import { Request, Response } from "express";
+const app = Express();
+app.use(cors());
+app.use(Express.json());
+
+let server: any;
+
+function startServer(port: number): Promise<Express.Application> {
+  return new Promise((resolve, reject) => {
+    server = app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+      resolve(app); // Resolve with the Express application instance
+    });
+
+    server.on("error", (err) => {
+      if ((err as NodeJS.ErrnoException).code === "EADDRINUSE") {
+        console.error(`Port ${port} is already in use`);
+        reject(err);
+      } else {
+        reject(err);
+      }
+    });
+  });
+}
+
+app.get("/ping", (req: Request, res: Response) => {
+  res.send("pong");
+});
+
+process.on("uncaughtException", (err) => {
+  console.error("Uncaught Exception:", err.stack);
+});
+
+process.on("unhandledRejection", (reason: any, promise) => {
+  console.error("Unhandled Rejection at:", promise, "reason:", reason.stack);
+});
+
+startServer(3000)
+  .catch(() => startServer(8081))
+  .catch((err) => {
+    console.error("Failed to start the server on both ports:", err);
+    process.exit(1);
+  });
